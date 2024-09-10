d62e38f831f8d405c8fff1d2cfe9e33cc6bc1392
Laurin Streng <l.streng@finatix.de>
2024-09-10 20:46:07 +0200

    Add .trunk to gitignore change var to :=

--- internal/api/calc.go
+++ internal/api/calc.go
@@ -3,9 +3,9 @@ package calc
 import "fmt"
 
 func Calculate() int {
-	var num1 = 1
-	var num2 = 1
-	var sum = num1 + num2
+	num1 := 1
+	num2 := 1
+	sum := num1 + num2
 	fmt.Println(sum)
 	return sum
 }

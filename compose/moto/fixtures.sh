aed05c3e7ee8e9f37b1e19da76e43b9809b1b8e0
Marcel Herhold <herhold.marcel@gmail.com>
2024-09-24 12:51:57 +0200

    #7 adding test script

--- compose/moto/fixtures.sh
+++ compose/moto/fixtures.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-aws ecs
+echo $PWD
+
+aws ecs create-cluster --cluster-name moto
+aws ecs register-task-definition --cli-input-json file://nginx.json
+aws ecs create-service \
+    --cluster moto \
+    --service-name nginx-service \
+    --task-definition nginx-fargate-task \
+    --desired-count 1 \
+    --launch-type FARGATE
